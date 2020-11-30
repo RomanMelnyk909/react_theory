19a8c971f5d6c67d79e7baa0440e587f13e5fb27
RomanMelnyk909 <45914004+RomanMelnyk909@users.noreply.github.com>
2020-11-30 21:02:42 +0200

    Learn JSX  syntaxys

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, {Component}  from 'react';
 import './App.css';
+import Car from './Car/Car'
+// function App() {
+//   const divStyle = {
+//     textAlign: 'center'
+//   }
+//   return (
+    
+//     <div  style= {divStyle}>
+//       <h1>Hello React</h1>
+//     </div>
+    
+//   );
+// }
 
-function App() {
+
+class App extends Component {
+  render() {
+    const divStyle = {
+    textAlign: 'center'
+  }
   return (
-    <div className="App">
-      <h1>Learn JSX</h1>
+    
+    <div  style= {divStyle}>
+      <h1>Hello React</h1>
+      <Car />
     </div>
+    
   );
+  }
 }
 
 export default App;
---
+++ src/Car/Car.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// function Car() {
+//     return (
+//         <div>This is car component</div>
+//     )
+// }
+// const car = () => {
+//     return (
+//         <div>This is car component</div>
+//     )
+// }
+
+// const car = () => (
+// <div>This is car component
+//     <strong>test</strong>
+// </div>
+// )
+
+
+
+export default () => (
+    <div>This is car component
+    <strong>test</strong>
+    </div>
+)
